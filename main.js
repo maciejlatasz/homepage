519182266d91edacece70995e02f11db9876f626
Maciej Latasz <macts@o2.pl>
2019-06-18 20:40:40 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('siema');
+console.log('Super tajne dane')
+const name = 'Maciek';
+const age = 29;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
