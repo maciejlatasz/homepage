3920f0c6deb8ac52dc999b5dbd76b6566a1fc572
Maciej Latasz <maciejlatasz@gmail.com>
2019-07-09 20:50:04 +0200

    BEM changes

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ const age = 29;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const heading = document.querySelector('.main__heading');
+
+console.log(heading);
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+
+const change = document.querySelector('.about__test');
+
+console.log(change.innerHTML);
+change.innerHTML = 'Zmiana wprowadzona za pomocą JS';
